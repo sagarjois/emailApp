ec54e85afa28a568830db3a60ff2549f1104b3ae
sjois91@gmail.com <sjois91@gmail.com>
2017-10-12 20:46:41 +0530

    Small change to keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 // keys.js - figure out what set of credentials to return
-if(process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
     module.exports = require('./prod');
 } else {
     module.exports = require('./dev');
