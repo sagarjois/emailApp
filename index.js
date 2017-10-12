2ff3208eff16be637e1335167f708f3f07bbb099
sjois91@gmail.com <sjois91@gmail.com>
2017-10-13 01:14:07 +0530

    Small change in index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
 
-mongoose.connect(keys.mongoURI, (err) => {
+mongoose.connect(keys.mongoURI, (err, db) => {
     if(err) {
         console.log(err);
     } else {
