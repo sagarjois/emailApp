9205168b5f57d2340d73777fa18671c0d67038f5
sjois91@gmail.com <sjois91@gmail.com>
2017-10-20 23:33:01 +0530

    Refactored billingRoutes

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -1,8 +1,10 @@
 const keys = require('../config/keys');
 const stripe = require('stripe')(keys.stripeSecretKey);
+const requireLogin = require('../middlewares/requireLogin');
 
 module.exports = app => {
-    app.post('/api/stripe', async (req, res) => {
+    app.post('/api/stripe', requireLogin, async (req, res) => {
+
         const charge = await stripe.charges.create({
             amount: 500,
             currency: 'usd',
@@ -12,7 +14,7 @@ module.exports = app => {
 
         req.user.credits += 5;
         const user = await req.user.save();
-        
+
         res.send(user);
     });
 };
