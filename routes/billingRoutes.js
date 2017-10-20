09d28ee788475b634570cd894645b1fc6513f8a9
sjois91@gmail.com <sjois91@gmail.com>
2017-10-20 19:53:05 +0530

    Added credit property to user schema

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -5,13 +5,14 @@ module.exports = app => {
     app.post('/api/stripe', async (req, res) => {
         const charge = await stripe.charges.create({
             amount: 500,
-            currency: usd,
+            currency: 'usd',
             description: '$5 for 5 credits',
             source: req.body.id
         });
 
         req.user.credits += 5;
         const user = await req.user.save();
+        
         res.send(user);
     });
 };
